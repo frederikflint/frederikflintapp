8516e924047101feba3fa266f018e9d695237c8d
Frederik Flint <frederikflint@gmail.com>
2020-10-20 22:25:33 +0200

    Add birthday countdown

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <nav class="flex items-center justify-between flex-wrap bg-green-900 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight"
+          >frederikflint.com</span
+        >
+      </div>
+    </nav>
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,95 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="text-base text-4xl sm:text-6xl">frederikflint.com</h1>
-      <h3>Velkommen til min hjemmeside</h3>
-      <p>Den kan ikke en skid</p>
+      <h1 class="title text-base text-4xl sm:text-6xl">
+        Tid til min fødselsdag
+      </h1>
+      <div class="justify-items-center">
+        <div
+          class="bg-green-900 m-8 time-box block md:inline-block rounded-lg text-white"
+        >
+          <p>{{ days }}</p>
+          <p>days</p>
+        </div>
+        <div
+          class="bg-green-900 m-8 time-box block md:inline-block rounded-lg text-white"
+        >
+          <p>{{ hours }}</p>
+          <p>hours</p>
+        </div>
+        <div
+          class="bg-green-900 m-8 time-box block md:inline-block rounded-lg text-white"
+        >
+          <p>{{ minutes }}</p>
+          <p>minutes</p>
+        </div>
+        <div
+          class="bg-green-900 m-8 time-box block md:inline-block rounded-lg text-white"
+        >
+          <p>{{ seconds }}</p>
+          <p>seconds</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      birthDay: {
+        day: 22,
+        month: 4,
+      },
+      seconds: 60,
+      minutes: 60,
+      hours: 24,
+      days: 999,
+    }
+  },
+  mounted() {
+    this.updateTime()
+    window.setInterval(() => this.updateTime(), 1000)
+  },
+  methods: {
+    updateTime() {
+      const { day: birthDay, month: birthMonth } = this.birthDay
+
+      const currentDate = new Date()
+      const currentMonth = currentDate.getMonth()
+
+      let year = currentDate.getFullYear()
+
+      // Check if we are past birthday this year
+      if (
+        currentMonth > birthMonth ||
+        (currentMonth === birthMonth && currentDate.getDay() > birthDay)
+      ) {
+        year += 1
+      }
+
+      const birthDate = new Date(year, birthMonth, birthDay)
+
+      const totalSecondsToBirthday = (birthDate - currentDate) / 1000
+
+      this.days = Math.floor(totalSecondsToBirthday / (60 * 60 * 24))
+      this.hours = Math.floor(
+        (totalSecondsToBirthday - this.days * 60 * 60 * 24) / (60 * 60)
+      )
+      this.minutes = Math.floor(
+        (totalSecondsToBirthday -
+          (this.hours * 60 * 60 + this.days * 60 * 60 * 24)) /
+          60
+      )
+
+      this.seconds = Math.floor(
+        totalSecondsToBirthday -
+          (this.minutes * 60 + this.hours * 60 * 60 + this.days * 60 * 60 * 24)
+      )
+    },
+  },
+}
 </script>
 
 <style>
@@ -23,29 +103,16 @@ export default {}
   min-height: 100vh;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   text-align: center;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  margin-top: 20px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.time-box {
+  text-align: center;
+  min-width: 70px;
 }
 </style>

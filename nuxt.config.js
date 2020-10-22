375608f275baeafdba9440f3b6af0f505be12e3c
Frederik Flint <frederikflint@gmail.com>
2020-10-22 21:36:54 +0200

    Add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)

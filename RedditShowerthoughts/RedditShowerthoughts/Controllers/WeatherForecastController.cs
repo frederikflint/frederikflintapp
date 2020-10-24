e9f7545d2c0df1c15fe63f45d052a25ba6d5610e
Frederik Flint <frederikflint@gmail.com>
2020-10-24 10:02:46 +0200

    Refactor + Add reddit shower thought api

---
+++ RedditShowerthoughts/RedditShowerthoughts/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RedditShowerthoughts.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class WeatherForecastController : ControllerBase
+  {
+    private readonly IHttpClientFactory _clientFactory;
+    private static readonly string[] Summaries = new[]
+    {
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory clientFactory)
+    {
+      _clientFactory = clientFactory;
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+      var client = _clientFactory.CreateClient();
+      var data = await client.GetAsync("http://www.reddit.com/r/showerthoughts/random.json");
+      Console.WriteLine(data);
+
+    }
+  }
+}
